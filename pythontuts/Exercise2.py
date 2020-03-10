cd8c5de0c77bf5a7e8093165dcda4ce2061c88f3
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-10 18:13:10 +0530

    Sixth commit

---
+++ pythontuts/Exercise2.py
@@ -0,0 +1,29 @@
+print("Enter the two numbers")
+a=int(input())
+b=int(input())
+
+print("Enter the operation you want to apply: 1 for *,2 for /,3 for +,4 for -")
+i=int(input())
+if(i==1 and a==45 and b==3):
+    print("555")
+elif(i==1):
+    c=a*b
+    print(c)
+if(i==3 and a==56 and b==9):
+    print("77")
+elif(i==3):
+    c=a+b
+    print(c)
+if (i == 2 and a == 56 and b == 4):
+    print("4")
+elif (i == 2):
+    c = a/b
+    print(c)
+if(i==4):
+    c=a-b
+    print(c)
+
+
+
+
+
---
+++ pythontuts/Exersice1.py
@@ -0,0 +1,4 @@
+d={"Jay":"01","Ajay":"02","Amar":"03"}
+print(d)
+i=input("enter key value\n")
+print(d[i])
