effb450f9b4064bc48eba0677d454ff1cd06c02c
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-10 14:49:07 +0530

    Third commit

--- pythontuts/tut6.py
+++ pythontuts/tut6.py
@@ -1,1 +1,2 @@
 print("Hello world"," I am here")
+print("\n How are you all" "\t Are you enjoying your life?")
---
+++ pythontuts/tut9.py
@@ -0,0 +1,17 @@
+
+grocery = ["Fruits","Vegetables","deo","Chocolates",30]
+print(grocery[0:5:2])
+
+Numbers = [1,5,2,7,4,8,9,45,1,25,6,2,78]
+print(Numbers)
+Numbers.sort()
+print(Numbers)
+Numbers.reverse()
+print((Numbers))
+
+print(len(Numbers))
+
+print(Numbers[0:13:2])
+Numbers.append(99)
+print(Numbers)
+
