c6504625e8c2c66325b7b8857fef404b4328858e
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-10 15:23:32 +0530

    Fourth commit

---
+++ pythontuts/tut10.py
@@ -0,0 +1,8 @@
+#Dictionary
+d1={"Jaydeep":"Burger","Rohan":"Pizza","Shubham":{"Break":"Bread","Lunch":"Paneer"}}
+print(d1)
+print(d1["Shubham"]["Break"])
+d1["Aditya"]="Junk food"
+d1["Mohan"]="Palak"
+del d1["Rohan"]
+print(d1)
