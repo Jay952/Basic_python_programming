e304628018439be02a00c71986d3f65830afca6d
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-13 22:55:08 +0530

    Tenth

---
+++ pythontuts/Functions.py
@@ -0,0 +1,24 @@
+a=5
+b=8
+c=sum([a,b])
+print(c)
+
+def function1(a,b):
+    print("You are in function1",a+b)
+print(function1(9,8))
+function1(9,9)
+
+def function2(a,b):
+    print("You are in function2")
+    c=(a+b)/2
+    print(c)
+
+function2(5,7)
+
+
+
+
+
+
+
+
---
+++ pythontuts/shorthand.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a:\n"))
+b=int(input("Enter b:\n"))
+
+#if a>b: print("a is greater than b")
+print("b is greater than a") if b>a else print("a is greater than b")
