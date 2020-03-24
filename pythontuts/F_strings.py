247b52581ad7cd8aa5c2e0d06e0e20ad01ae827f
[Jay952] <[jaydeep4420@gmail.com]>
2020-03-24 11:31:39 +0530

    python contimue

---
+++ pythontuts/F_strings.py
@@ -0,0 +1,14 @@
+import math
+me="Jaydeep"
+you ="Rahul"
+a="this is %s and this is %s"%(me,you)
+print(a)
+
+
+b="this is {} {}"
+c=b.format(me,you)
+print(c)
+
+
+k=f"Tih is {me} {you} {math.cos(90)}"
+print(k)
---
+++ pythontuts/using.py
@@ -0,0 +1,16 @@
+import random
+import sklearn
+random_number=random.randint(0,9)
+print(random_number)
+
+
+rand=random.random()*40
+print(rand)
+
+lst=["Star plus","Star gold","Cartoon Network","Aaj Tak","Its Jaydeephere"]
+choice=random.choice(lst)
+print(choice)
+
+lst1=['1','2','3','4','5','6']
+dice=random.choice(lst1)
+print(dice)
